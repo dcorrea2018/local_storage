aab64e3f97635cd5c044d132b90b242bbf2e0894
dcorrea2018 <38849053+dcorrea2018@users.noreply.github.com>
2022-08-20 11:04:06 -0700

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Entro al main.js");
+
+
+const titulo = document.getElementById("titulo");
+let color = localStorage.getItem("colorPagina");
+console.log(color);
+if (color == null) {
+    color = "pink";
+}
+
+
+titulo.style.color = color;
+
+localStorage.clear();
